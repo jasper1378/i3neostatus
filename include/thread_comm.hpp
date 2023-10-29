358b93242c85bc5b1321f5becc72890642c9b8c2
Jasper Young <jasper.c.young@gmail.com>
2023-10-28 20:47:58 -0400

    Add callback func for state change to thread comm

--- include/thread_comm.hpp
+++ include/thread_comm.hpp
@@ -3,6 +3,7 @@
 
 #include <atomic>
 #include <compare>
+#include <concepts>
 #include <cstddef>
 #include <exception>
 #include <iostream>
@@ -13,6 +14,16 @@
 namespace thread_comm {
 // TODO reuse buffer
 
+namespace shared_state_state {
+using type = unsigned int;
+enum : type {
+  NONE = 0b000,
+  READ = 0b001,
+  UNREAD = 0b010,
+  EXCEPTION = 0b100,
+};
+
+} // namespace shared_state_state
 template <typename t_value> class shared_state;
 template <typename t_value> class shared_state_ptr;
 template <typename t_value> class producer;
@@ -20,30 +31,37 @@ template <typename t_value> class consumer;
 
 template <typename t_value> class shared_state {
 public:
+  using t_state_change_callback_func = void (*)(shared_state_state::type);
+
+public:
   friend class shared_state_ptr<t_value>;
 
 private:
-  enum class STATE {
-    READ = 0,
-    UNREAD = 1,
-    EXCEPTION = 2,
-  };
-
-private:
-  std::atomic<STATE> m_state;
+  std::atomic<shared_state_state::type> m_state;
   std::atomic<t_value *> m_value;
   std::exception_ptr m_exception;
+  t_state_change_callback_func m_state_change_callback_func;
+  shared_state_state::type m_state_change_subscribed_events;
 
 private:
   shared_state()
-      : m_state{STATE::READ}, m_value{nullptr}, m_exception{nullptr} {}
+      : m_state{shared_state_state::READ}, m_value{nullptr},
+        m_exception{nullptr}, m_state_change_callback_func{nullptr},
+        m_state_change_subscribed_events{shared_state_state::NONE} {}
+
+  shared_state(t_state_change_callback_func state_change_callback_func,
+               shared_state_state::type state_change_subscribed_events)
+      : m_state{shared_state_state::READ}, m_value{nullptr},
+        m_exception{nullptr},
+        m_state_change_callback_func{state_change_callback_func},
+        m_state_change_subscribed_events{state_change_subscribed_events} {}
 
   shared_state(const shared_state &other) = delete;
 
   shared_state(shared_state &&other) = delete;
 
   ~shared_state() {
-    m_state.store(STATE::READ);
+    m_state.store(shared_state_state::READ);
     if (m_value.load()) {
       delete m_value.load();
       m_value.store(nullptr);
@@ -56,10 +74,13 @@ private:
 
 public:
   bool set_value(std::unique_ptr<t_value> value) {
-    if (m_state.load() != STATE::EXCEPTION) {
+    if (m_state.load() != shared_state_state::EXCEPTION) {
       t_value *old_value{m_value.exchange(value.release())};
-      m_state.store(STATE::UNREAD);
+      m_state.store(shared_state_state::UNREAD);
       m_state.notify_all();
+      if (m_state_change_subscribed_events & shared_state_state::UNREAD) {
+        m_state_change_callback_func(shared_state_state::UNREAD);
+      }
       if (old_value) {
         delete old_value;
         old_value = nullptr;
@@ -71,10 +92,13 @@ public:
   }
 
   bool set_exception(std::exception_ptr exception) {
-    if (m_state.load() != STATE::EXCEPTION) {
+    if (m_state.load() != shared_state_state::EXCEPTION) {
       m_exception = exception;
-      m_state.store(STATE::EXCEPTION);
+      m_state.store(shared_state_state::EXCEPTION);
       m_state.notify_all();
+      if (m_state_change_subscribed_events & shared_state_state::EXCEPTION) {
+        m_state_change_callback_func(shared_state_state::EXCEPTION);
+      }
       return true;
     } else {
       return false;
@@ -83,15 +107,18 @@ public:
 
   std::unique_ptr<t_value> get() {
     switch (m_state.load()) {
-    case STATE::READ: {
+    case shared_state_state::READ: {
       wait();
       return get();
     } break;
-    case STATE::UNREAD: {
-      m_state.store(STATE::READ);
+    case shared_state_state::UNREAD: {
+      m_state.store(shared_state_state::READ);
+      if (m_state_change_subscribed_events & shared_state_state::READ) {
+        m_state_change_callback_func(shared_state_state::READ);
+      }
       return std::unique_ptr<t_value>{m_value.exchange(nullptr)};
     } break;
-    case STATE::EXCEPTION: {
+    case shared_state_state::EXCEPTION: {
       std::rethrow_exception(m_exception);
     } break;
     default: {
@@ -100,11 +127,15 @@ public:
     }
   }
 
-  void wait() { m_state.wait(STATE::READ); }
+  void wait() { m_state.wait(shared_state_state::READ); }
 };
 
 template <typename t_value> class shared_state_ptr {
 public:
+  friend std::pair<producer<t_value>, consumer<t_value>>
+      make_thread_comm_pair<t_value>(
+          typename shared_state<t_value>::t_state_change_callback_func,
+          shared_state_state::type);
   friend std::pair<producer<t_value>, consumer<t_value>>
   make_thread_comm_pair<t_value>();
 
@@ -339,11 +370,23 @@ public:
 };
 
 template <typename t_value>
+std::pair<producer<t_value>, consumer<t_value>> make_thread_comm_pair(
+    typename shared_state<t_value>::t_state_change_callback_func
+        state_change_callback_func,
+    shared_state_state::type state_change_subscribed_events) {
+  shared_state_ptr<t_value> ssp{
+      shared_state_ptr<t_value>::make_shared_state_ptr(
+          state_change_callback_func, state_change_subscribed_events)};
+  return std::make_pair<producer<t_value>, consumer<t_value>>(ssp, ssp);
+}
+
+template <typename t_value>
 std::pair<producer<t_value>, consumer<t_value>> make_thread_comm_pair() {
   shared_state_ptr<t_value> ssp{
       shared_state_ptr<t_value>::make_shared_state_ptr()};
   return std::make_pair<producer<t_value>, consumer<t_value>>(ssp, ssp);
 }
+
 } // namespace thread_comm
 
 #endif
