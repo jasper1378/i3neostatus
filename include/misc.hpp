7ca135e4f12e912af0272bf704441524b79f9326
Jasper Young <jasper.c.young@gmail.com>
2023-12-22 22:45:23 -0500

    Create constexpr variadic min()/max() functions

--- include/misc.hpp
+++ include/misc.hpp
@@ -5,6 +5,20 @@
 
 namespace misc {
 static constexpr std::string g_k_program_name{"i3neostatus"};
+
+namespace constexpr_minmax {
+template <typename T> consteval auto max(const T &t) { return t; }
+template <typename T, typename... U>
+consteval auto max(const T &t, const U &...u) {
+  return ((t > max(u...)) ? (t) : (max(u...)));
+}
+template <typename T> constexpr auto min(const T &t) { return t; }
+template <typename T, typename... U>
+constexpr auto min(const T &t, const U &...u) {
+  return ((t < min(u...)) ? (t) : (min(u...)));
+}
+} // namespace constexpr_minmax
+
 } // namespace misc
 
 #endif
