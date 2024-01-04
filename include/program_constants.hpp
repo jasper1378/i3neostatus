bbcea8628d41d0dab1d6e08b06c3827e999c92cb
Jasper Young <jasper.c.young@gmail.com>
2024-01-04 18:47:34 -0500

    Create "program constants" header

---
+++ include/program_constants.hpp
@@ -0,0 +1,11 @@
+#ifndef PROGRAM_CONSTANTS_HPP
+#define PROGRAM_CONSTANTS_HPP
+
+#include <string>
+
+namespace program_constants {
+static constexpr std::string g_k_program_name{"i3neostatus"};
+static constexpr std::string g_k_program_version{"0"};
+}; // namespace program_constants
+
+#endif
