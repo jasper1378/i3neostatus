f6e7d1f614f4227365d4aacd229b98bd01522cff
Jasper Young <jasper.c.young@gmail.com>
2024-01-04 19:32:54 -0500

    Add stuff to program_constants

--- include/program_constants.hpp
+++ include/program_constants.hpp
@@ -4,8 +4,10 @@
 #include <string>
 
 namespace program_constants {
-static constexpr std::string g_k_program_name{"i3neostatus"};
-static constexpr std::string g_k_program_version{"0"};
+static constexpr std::string g_k_name{"i3neostatus"};
+static constexpr std::string g_k_version{"0"};
+static constexpr int g_k_year{2024};
+static constexpr std::string g_k_authors{"Jasper Young"};
 }; // namespace program_constants
 
 #endif
