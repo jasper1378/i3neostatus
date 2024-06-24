86928cd6d07ed9992987bb778d4acc21a36c7320
Jasper Young <jasper.c.young@gmail.com>
2024-06-23 23:01:16 -0400

    Add prefix to some constant identifiers

--- source/i3bar_data_conversions.cpp
+++ source/i3bar_data_conversions.cpp
@@ -13,8 +13,8 @@ std::string i3neostatus::i3bar_data::types::to_string<
     i3neostatus::i3bar_data::types::text_align>(const text_align value) {
   static constexpr std::array<std::string,
                               static_cast<std::size_t>(text_align::max)>
-      lut{"", "center", "right", "left"};
-  return lut[static_cast<std::size_t>(value)];
+      k_lut{"", "center", "right", "left"};
+  return k_lut[static_cast<std::size_t>(value)];
 }
 
 template <>
@@ -22,8 +22,8 @@ std::string i3neostatus::i3bar_data::types::to_string<
     i3neostatus::i3bar_data::types::markup>(const markup value) {
   static constexpr std::array<std::string,
                               static_cast<std::size_t>(markup::max)>
-      lut{"", "pango"};
-  return lut[static_cast<std::size_t>(value)];
+      k_lut{"", "pango"};
+  return k_lut[static_cast<std::size_t>(value)];
 }
 
 template <>
@@ -31,38 +31,38 @@ i3neostatus::i3bar_data::types::click_modifiers
 i3neostatus::i3bar_data::types::from_string<
     i3neostatus::i3bar_data::types::click_modifiers>(
     const std::string &string) {
-  static constexpr std::string str_mod1{"Mod1"};
-  static constexpr std::string str_mod2{"Mod2"};
-  static constexpr std::string str_mod3{"Mod3"};
-  static constexpr std::string str_mod4{"Mod4"};
-  static constexpr std::string str_mod5{"Mod5"};
-  static constexpr std::string str_shift{"Shift"};
-  static constexpr std::string str_control{"Control"};
-  static constexpr std::string str_lock{"Lock"};
+  static constexpr std::string k_str_mod1{"Mod1"};
+  static constexpr std::string k_str_mod2{"Mod2"};
+  static constexpr std::string k_str_mod3{"Mod3"};
+  static constexpr std::string k_str_mod4{"Mod4"};
+  static constexpr std::string k_str_mod5{"Mod5"};
+  static constexpr std::string k_str_shift{"Shift"};
+  static constexpr std::string k_str_control{"Control"};
+  static constexpr std::string k_str_lock{"Lock"};
 
   switch (misc::constexpr_hash_string::hash(string)) {
-  case misc::constexpr_hash_string::hash(str_mod1): {
+  case misc::constexpr_hash_string::hash(k_str_mod1): {
     return click_modifiers::mod1;
   } break;
-  case misc::constexpr_hash_string::hash(str_mod2): {
+  case misc::constexpr_hash_string::hash(k_str_mod2): {
     return click_modifiers::mod2;
   } break;
-  case misc::constexpr_hash_string::hash(str_mod3): {
+  case misc::constexpr_hash_string::hash(k_str_mod3): {
     return click_modifiers::mod3;
   } break;
-  case misc::constexpr_hash_string::hash(str_mod4): {
+  case misc::constexpr_hash_string::hash(k_str_mod4): {
     return click_modifiers::mod4;
   } break;
-  case misc::constexpr_hash_string::hash(str_mod5): {
+  case misc::constexpr_hash_string::hash(k_str_mod5): {
     return click_modifiers::mod5;
   } break;
-  case misc::constexpr_hash_string::hash(str_shift): {
+  case misc::constexpr_hash_string::hash(k_str_shift): {
     return click_modifiers::shift;
   } break;
-  case misc::constexpr_hash_string::hash(str_control): {
+  case misc::constexpr_hash_string::hash(k_str_control): {
     return click_modifiers::control;
   } break;
-  case misc::constexpr_hash_string::hash(str_lock): {
+  case misc::constexpr_hash_string::hash(k_str_lock): {
     return click_modifiers::lock;
   } break;
   default: {
